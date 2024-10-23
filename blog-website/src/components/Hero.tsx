86c1a99de85000a71eb21ef7ce167a0cfbcf46a9
faizisheikh700 <faizisheikh700@gmail.com>
2024-10-24 01:34:13 +0500

    blog

--- blog-website/src/components/Hero.tsx
+++ blog-website/src/components/Hero.tsx
@@ -1,13 +1,18 @@
 export default function Hero(){
     return (
         <>
-        
+            <div>
+
             <div className="heroImage">
+                <div className="mainImage">
+
                 <img src="" alt="#" />
+                </div>
                     <div className="centerText">
                         <h1>Books Blogs</h1>
                         <p>Books play a very important role in our life </p>
                     </div>
+            </div>
             </div>
             
         
